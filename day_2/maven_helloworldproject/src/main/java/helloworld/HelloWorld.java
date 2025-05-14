f16572fc36dffe4e234ad66d84ff6f0a4a73593b
azdan-hand <azdan.vila@hand-global.com>
2025-05-14 15:22:20 +0700

    create hello world example Maven project

---
+++ day_2/maven_helloworldproject/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package day_2.maven_helloworldproject.src.main.java.helloworld;
+
+public class HelloWorld {
+    public static void main(String args[]){
+            System.out.println("Hello World, Maven");
+    }
+}
