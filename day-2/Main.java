031a0a0da4d45004c13346485d381babff9818e8
azdan-hand <azdan.vila@hand-global.com>
2025-05-14 11:13:26 +0700

    create myObj object in Main class

--- day-2/Main.java
+++ day-2/Main.java
@@ -1,4 +1,11 @@
-// FileName: "Main.java"
+// Main class
 public class Main {
+    // assigning the integer value 5 to the variable x
     int x = 5;
+
+    // myObj object
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+    }
 }
