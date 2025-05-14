2efb996f89555d5d08d5523109d4178637e947ea
azdan-hand <azdan.vila@hand-global.com>
2025-05-14 11:27:26 +0700

    create multiple objects

---
+++ day-2/java-multipleobjects/Main.java
@@ -0,0 +1,13 @@
+// Main class
+public class Main {
+    // assigning the integer value 5 to the variable x
+    int x = 5;
+
+    // multiple objects (myObj1 and myObj2)
+    public static void main(String[] args) {
+        Main myObj1 = new Main();
+        Main myObj2 = new Main();
+        System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+    }
+}
