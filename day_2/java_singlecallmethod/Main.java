847dcc0adc1ac27226c52607dced8c8e59b54e65
azdan-hand <azdan.vila@hand-global.com>
2025-05-14 14:12:17 +0700

    call method in single time

---
+++ day_2/java_singlecallmethod/Main.java
@@ -0,0 +1,13 @@
+package day_2.java_singlecallmethod;
+
+public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
+
+// Outputs "I just got executed!"
