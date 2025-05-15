7192f46296d7ea206575d67c8ca2c5e20cd13a4e
azdan-hand <azdan.vila@hand-global.com>
2025-05-15 11:54:51 +0700

    create a service based on Java (zcap_sqllite)

---
+++ day_3/zcap_sqllite/srv/src/main/java/com/azdan/cap/zcap_sqllite/Application.java
@@ -0,0 +1,13 @@
+package com.azdan.cap.zcap_sqllite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
